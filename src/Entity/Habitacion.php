5a9ab3b06043ffd8866d051505940d29176c129e
Jao <jaortegadur@gmail.com>
2024-03-08 09:31:48 +0100

    [+] tabla habitacion

---
+++ src/Entity/Habitacion.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HabitacionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: HabitacionRepository::class)]
+class Habitacion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nombre = null;
+
+    #[ORM\Column]
+    private ?int $Numero = null;
+
+    #[ORM\Column]
+    private ?int $Capacidad = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->Nombre;
+    }
+
+    public function setNombre(string $Nombre): static
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->Numero;
+    }
+
+    public function setNumero(int $Numero): static
+    {
+        $this->Numero = $Numero;
+
+        return $this;
+    }
+
+    public function getCapacidad(): ?int
+    {
+        return $this->Capacidad;
+    }
+
+    public function setCapacidad(int $Capacidad): static
+    {
+        $this->Capacidad = $Capacidad;
+
+        return $this;
+    }
+}
