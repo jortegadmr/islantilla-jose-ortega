abba436220ace6ae4e7a8e83c349fca556075b0d
Jao <jaortegadur@gmail.com>
2024-03-11 12:24:26 +0100

    [+] Funcionan las Fixtures

--- src/DataFixtures/ActividadesFixtures.php
+++ src/DataFixtures/ActividadesFixtures.php
@@ -12,12 +12,12 @@ class ActividadesFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $actividadesData = [
-            ['Centro SPA & Wellness', 'SPA', '2024-03-16', '2024-03-16', 2],
-            ['GOLF', 'Deporte', '2024-03-16', '2024-03-16', 3],
-            ['Buffet Ostras', 'Restaurantes', '2024-03-17', '2024-03-19', 4],
-            ['Fandado Bar Restaurante', 'Restaurantes', '2024-04-07', '2024-04-07', 5],
-            ['Pool Bar', 'Restaurantes', '2024-03-22', '2024-03-23', 6],
-            ['Kiosko Bar', 'Restaurantes', '2024-03-30', '2024-03-30', 7]
+            ['Centro SPA & Wellness', 'SPA', '2024-03-16', '2024-03-16'],
+            ['GOLF', 'Deporte', '2024-03-16', '2024-03-16'],
+            ['Buffet Ostras', 'Restaurantes', '2024-03-17', '2024-03-19'],
+            ['Fandado Bar Restaurante', 'Restaurantes', '2024-04-07', '2024-04-07'],
+            ['Pool Bar', 'Restaurantes', '2024-03-22', '2024-03-23'],
+            ['Kiosko Bar', 'Restaurantes', '2024-03-30', '2024-03-30']
         ];
 
         foreach ($actividadesData as $actividadData) {
@@ -26,13 +26,13 @@ class ActividadesFixtures extends Fixture
             $actividad->setCategoria($actividadData[1]);
             $actividad->setFechaInicio(new \DateTime($actividadData[2]));
             $actividad->setFechaFin(new \DateTime($actividadData[3]));
-            $idUsuario = $actividadData[4];
+            /* $idUsuario = $actividadData[4];
             $usuario = $manager->getRepository(Usuario::class)->find($idUsuario);
             if ($usuario) {
                 $actividad->addIdUsuario($usuario); // Utiliza el método addIdUsuario() para agregar el usuario
             } else {
-                throw new \Exception("No se pudo encontrar el usuario con ID: $idUsuario");
-            }
+                //throw new \Exception("No se pudo encontrar el usuario con ID: $idUsuario");
+            } */
 
             $manager->persist($actividad);
         }
--- src/DataFixtures/HabitacionFixtures.php
+++ src/DataFixtures/HabitacionFixtures.php
@@ -3,30 +3,40 @@
 namespace App\DataFixtures;
 
 use App\Entity\Habitacion;
+use App\Entity\Usuario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HabitacionFixtures extends Fixture
 {
+    private EntityManagerInterface $entityManager;
+    private \Doctrine\Persistence\ObjectRepository $usuarioRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->usuarioRepository = $this->entityManager->getRepository(Usuario::class);
+    }
     public function load(ObjectManager $manager)
     {
         $habitacionesData = [
-            ['Habitación Estandar', 2, 1001, 4],
-            ['Habitación Estandar', 2, 1002, null],
-            ['Habitación Estandar', 2, 1003, null],
-            ['Habitación Estandar', 2, 1004, null],
-            ['Habitación Deluxe', 2, 2001, 3],
-            ['Habitación Deluxe', 2, 2002, 8],
-            ['Habitación Deluxe', 2, 2003, null],
-            ['Habitación Deluxe', 2, 2004, null],
-            ['Apartamento Premium', 4, 3001, null],
-            ['Apartamento Premium', 4, 3002, 1],
-            ['Apartamento Premium', 4, 3003, null],
-            ['Apartamento Premium', 4, 3004, null],
-            ['Apartamento Luxury', 4, 4001, 2],
-            ['Apartamento Luxury', 4, 4002, null],
-            ['Apartamento Luxury', 4, 4003, null],
-            ['Apartamento Luxury', 4, 4004, null]
+            ['Habitación Estandar', 2, 1001],
+            ['Habitación Estandar', 2, 1002],
+            ['Habitación Estandar', 2, 1003],
+            ['Habitación Estandar', 2, 1004],
+            ['Habitación Deluxe', 2, 2001],
+            ['Habitación Deluxe', 2, 2002],
+            ['Habitación Deluxe', 2, 2003],
+            ['Habitación Deluxe', 2, 2004],
+            ['Apartamento Premium', 4, 3001],
+            ['Apartamento Premium', 4, 3002],
+            ['Apartamento Premium', 4, 3003],
+            ['Apartamento Premium', 4, 3004],
+            ['Apartamento Luxury', 4, 4001],
+            ['Apartamento Luxury', 4, 4002],
+            ['Apartamento Luxury', 4, 4003],
+            ['Apartamento Luxury', 4, 4004]
         ];
 
         foreach ($habitacionesData as $habitacionData) {
@@ -34,10 +44,18 @@ class HabitacionFixtures extends Fixture
             $habitacion->setNombre($habitacionData[0]);
             $habitacion->setCapacidad($habitacionData[1]);
             $habitacion->setNumero($habitacionData[2]);
-            $habitacion->setUsuario($habitacionData[3]);
+            // Obtener el usuario por su ID
+           /*  $idUsuario = $habitacionData[3];
+            $usuario = $this->usuarioRepository->find($idUsuario); */
+
+            /* if ($usuario) {
+                $habitacion->setUsuario($usuario);
+            } else {
+                //throw new \Exception("No se pudo encontrar el usuario con ID: $idUsuario");
+            } */
 
             $manager->persist($habitacion);
-        }
+                }
 
         $manager->flush();
     }
